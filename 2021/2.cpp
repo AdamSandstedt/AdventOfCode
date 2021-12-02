5756d361df2b4684c0f036f78755c9f4f7eabc50
Adam Sandstedt <Adam.sandstedt@gmail.com>
2021-12-01 22:32:59 -0700

    Cleaned up the #includes for day 2

--- 2021/2.cpp
+++ 2021/2.cpp
@@ -1,22 +1,5 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <cstring>
-#include <iomanip>
-#include <cmath>
-#include <algorithm>
-#include <tuple>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <deque>
-#include <list>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-
-#include "split.h"
 
 using namespace std;
 
